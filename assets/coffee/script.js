1bf6c58a8f8e0e3e705b96530281d74cf9beb2a3
Jonathan Olier <djom202@gmail.com>
2016-08-31 16:28:48 -0500

    chore(the user adds drinks to orders): add the functions and the modal functionality

--- assets/coffee/script.js
+++ assets/coffee/script.js
@@ -1,14 +1,5 @@
 'use strict';
 
 $(function() {
-    // Your code here
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        changeForm();
-    } else {}
+    $('select').material_select();
 });
-
-function changeForm() {
-    setTimeout(function() {
-        // scripts
-    }, 1000);
-}
--- modules/controllers/DashboardCtrl.js
+++ modules/controllers/DashboardCtrl.js
@@ -4,7 +4,11 @@ Appc.controller('DashboardCtrl', ['$scope', '$http', '$location',
     function($scope, $http, $location) {
         console.log("Init Controller DashboardCtrl");
 
+        $('select').material_select();
+
         $scope.orders = [];
+        $scope.drinkId = 1;
+        $scope.amount = 1;
 
         // $scope.orders = [{
         //     'id': 1,
@@ -39,22 +43,21 @@ Appc.controller('DashboardCtrl', ['$scope', '$http', '$location',
             'price': 6
         }, {
             'id': 2,
-            'name': 'Juice',
-            'price': 8
+            'name': 'Coffee',
+            'price': 6
         }, {
             'id': 3,
             'name': 'Expresso',
             'price': 4
         }, {
             'id': 4,
-            'name': 'Martini',
-            'price': 10
+            'name': 'Juice',
+            'price': 8
         }, {
             'id': 5,
-            'name': 'Coffee',
-            'price': 6
+            'name': 'Martini',
+            'price': 10
         }];
-
 
         $scope.sumTotal = function() {
             var totals = 0;
@@ -66,28 +69,59 @@ Appc.controller('DashboardCtrl', ['$scope', '$http', '$location',
             return totals;
         };
 
-        $scope.addOrder = function(_orderId) {
-            var found = false;
-            angular.forEach($scope.orders, function(item, key) {
-                if (item.id === _orderId) {
-                    item.count++;
-                    found = true;
-                    return;
-                }
-            });
+        $scope.addOrder = function() {
+            var _found = false,
+                _drinkId = parseInt($scope.drinkId),
+                _amount = parseInt($scope.amount);
 
-            if (!found) {
-                var tempDrink = $scope.searchDrinks(_orderId);
-                $scope.orders.push(tempDrink);
+            console.log('_drinkId', $scope.drinkId);
+            console.log('_amount', _amount);
+
+            if (typeof _amount !== 'undefined' && typeof _drinkId !== 'undefined') {
+                angular.forEach($scope.orders, function(item, key) {
+                    if (item.id === _drinkId) {
+                        item.count += _amount;
+                        _found = true;
+                        return;
+                    }
+                });
+
+                if (!_found) {
+                    var tempDrink = $scope.searchDrinks(_drinkId);
+                    tempDrink.count = _amount;
+                    $scope.orders.push(tempDrink);
+                }
+
+                $scope.closeModal('#modal1');
             }
         };
 
         $scope.searchDrinks = function(_idDrink) {
-            angular.forEach($scope.drinks, function(item, key) {
-                if (item.id === _idDrink) {
-                    return item;
+            var item = null;
+
+            angular.forEach($scope.drinks, function(_item, key) {
+                if (_item.id === _idDrink) {
+                    console.log('found Drink', _idDrink);
+                    console.log('drink', _item);
+                    item = _item;
+                    return;
                 }
             });
+
+            return item;
+        };
+
+        $scope.openModal = function(_id) {
+            $(_id).openModal();
+        };
+
+        $scope.closeModal = function(_id) {
+            $(_id).closeModal();
+        };
+
+        $scope.initValues = function() {
+            $scope.amount = 1;
+            $scope.drinkId = 1;
         };
     }
 ]);
